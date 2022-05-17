8af9a2969d66af7516f8997183180a251233b310
Arturas <arturas.norvaishas@webmeridian.org>
2022-05-17 17:37:32 +0300

    'add deploy shell script'

---
+++ bin/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+bin/magento maintenance:enable
+rm -rf generated/*
+rm -rf var/page_cache/*
+rm -rf var/cache/*
+rm -rf var/view_preprocessed/*
+rm -rf pub/static/*
+bin/magento setup:upgrade
+bin/magento setup:di:compile
+bin/magento s:s:d -f --jobs=2
+bin/magento c:c
+bin/magento c:f
+bin/magento maintenance:disable
+
